59b5ee69e5d3c292fea8eb4ec9835526751f4941
Sanskar-211 <agarwalsanskar211@gmail.com>
2026-04-16 22:08:44 +0530

    added the intial javascript file to design application

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { sequelize } = require('./models');
+
+const subjectRoutes = require('./routes/subject');
+const timetableRoutes = require('./routes/timetable');
+const attendanceRoutes = require('./routes/attendance');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/subjects', subjectRoutes);
+app.use('/timetable', timetableRoutes);
+app.use('/attendance', attendanceRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Attendance Tracker Backend is running.');
+});
+
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}); 
